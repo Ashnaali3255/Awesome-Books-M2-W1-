fffd4b7b33ffa309bca27e95dd92b04492d5b80e
Ashnaali3255 <ashnaalipk@gmail.com>
2023-02-13 19:11:18 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,36 +1,54 @@
-const bookCollection = [
-    {
-      title: "To Kill a Mockingbird",
-      author: "Harper Lee",
-    },
-    {
-      title: "Pride and Prejudice",
-      author: "Jane Austen",
-    },
-    {
-      title: "The Catcher in the Rye",
-      author: "J.D. Salinger",
-    },
-    {
-      title: "The Great Gatsby",
-      author: "F. Scott Fitzgerald",
-    }
-  ];
-  const result = bookCollection.filter(bookCollection => bookCollection.length > 4);
-  
-  function Book(title, author) {
-    this.title = title;
-    this.author = author;
+// Collection of books
+let books = [];
+// Load books from local storage
+const loadBooks = () => {
+  const storedBooks = localStorage.getItem('books');
+  if (storedBooks) {
+    books = JSON.parse(storedBooks);
   }
-  
-  Book.prototype.toString = function() {
-    return this.title + 'by' + this.author
-  
+};
+// Save books to local storage
+const saveBooks = () => {
+  localStorage.setItem('books', JSON.stringify(books));
+};
+// Display books in the collection
+const displayBooks = () => {
+  const bookCollection = document.querySelector('#book-collection');
+  bookCollection.innerHTML = '';
+  for (const book of books) {
+    const bookEl = document.createElement('div');
+    bookEl.classList.add('book');
+    bookEl.innerHTML = `
+      <span>${book.title}<br> ${book.author}</span>
+      <button data-index="${book.index}">Remove</button>
+    `;
+    bookCollection.appendChild(bookEl);
   }
-  Book.prototype.print = function() {
-    console.log(this.toString());
+};
+// Add a book to the collection
+const addBook = () => {
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  books.push({
+    index: books.length,
+    title: title,
+    author: author
+  });
+  saveBooks();
+  displayBooks();
+};
+// Remove a book from the collection
+const removeBook = (index) => {
+  books = books.filter(book => book.index !== index);
+  saveBooks();
+  displayBooks();
+};
+// Handle "Add" button click
+document.querySelector('#add-book').addEventListener('click', addBook);
+// Handle "Remove" button click
+document.querySelector('#book-collection').addEventListener('click', (event) => {
+  if (event.target.tagName === 'BUTTON') {
+    removeBook(parseInt(event.target.dataset.index));
   }
-  
-  var  Awesome= new Book('Hamlet ', ' William Shakespeare');
-  
-  Awesome.print()
+});
+loadBooks
